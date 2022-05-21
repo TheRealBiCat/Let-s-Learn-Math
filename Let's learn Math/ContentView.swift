5f50f22deaef4051d6f93b87bd5000f3714b0559
Alex Bui <buingocan2105@gmail.com>
2022-05-21 20:08:37 +0700

    Initial Commit

---
+++ Let's learn Math/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Let's learn Math
+//
+//  Created by Alex Bùi on 21/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Let's learn Math/Let_s_learn_MathApp.swift
@@ -0,0 +1,17 @@
+//
+//  Let_s_learn_MathApp.swift
+//  Let's learn Math
+//
+//  Created by Alex Bùi on 21/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct Let_s_learn_MathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
